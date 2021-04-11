335f0c51252fc63945eb2776c774798ad918b0f2
Zinoviev Artem <7binary@gmail.com>
2021-04-11 19:43:34 +0300

    Updates

--- client/src/components/Preview/index.tsx
+++ client/src/components/Preview/index.tsx
@@ -47,7 +47,6 @@ const html = `
     font-size: 15px;
   }
   pre {
-    background-color: beige;
     padding: 4px 8px;
     font-size: 13px;
     white-space: pre-wrap;       /* css-3 */
@@ -63,8 +62,59 @@ const html = `
   .err h3 {
     margin-top: 0;
   }
+  /* console logger */
+  #log-container { overflow: auto; height: 150px; }
+  .log-warn { color: orange }
+  .log-error { color: red }
+  .log-info { color: skyblue }
+  .log-log { color: silver }
+  .log-warn, .log-error { font-weight: bold; }
 </style>
 <script>
+  // чтоб был console вывод в превьюхе
+  rewireLoggingToElement(
+    () => document.getElementById('log'),
+    () => document.getElementById('log-container'), true);
+  
+  function rewireLoggingToElement(eleLocator, eleOverflowLocator, autoScroll) {
+    fixLoggingFunc('log');
+    fixLoggingFunc('debug');
+    fixLoggingFunc('warn');
+    fixLoggingFunc('error');
+    fixLoggingFunc('info');
+  
+    function fixLoggingFunc(name) {
+      console['old' + name] = console[name];
+      console[name] = function(...arguments) {
+        const output = produceOutput(name, arguments);
+        const eleLog = eleLocator();
+  
+        if (autoScroll) {
+          const eleContainerLog = eleOverflowLocator();
+          const isScrolledToBottom = eleContainerLog.scrollHeight - eleContainerLog.clientHeight <= eleContainerLog.scrollTop + 1;
+          eleLog.innerHTML += output + '<br>';
+          if (isScrolledToBottom) {
+            eleContainerLog.scrollTop = eleContainerLog.scrollHeight - eleContainerLog.clientHeight;
+          }
+        } else {
+          eleLog.innerHTML += output + '<br>';
+        }
+  
+        console['old' + name].apply(undefined, arguments);
+      };
+    }
+  
+    function produceOutput(name, args) {
+      return args.reduce((output, arg) => {
+        return output +
+          '<span class="log-' + (typeof arg) + ' log-' + name + '">' +
+          (typeof arg === 'object' && (JSON || {}).stringify ? JSON.stringify(arg) : arg) +
+          '</span>&nbsp;';
+      }, '');
+    }
+  }
+
+  // чтоб показывать ошибки в превьюхе
   const handleError = (err) => {
     const root = document.querySelector('#root');
     root.innerHTML = '<div class="err"><h3>Runtime error</h3>' + err.toString() + '</div>';
@@ -73,14 +123,14 @@ const html = `
 
   window.addEventListener('message', (event) => {
     const [code, transpiled, error] = event.data.split('@*@');
-    // console.log('=> <iframe> for event with code length', code.length);
     
     if (error && error.length > 0) {
        return handleError(error);
     }
 
-    document.body.innerHTML = '<div id="root"></div>';
-    const root = document.querySelector('#root');
+    document.body.innerHTML = '<div id="root"></div>'
+      + '<div id="log-container"><pre id="log"></pre></div>'
+      + '<div id="transpiled-container><pre id="transpiled"></pre></div>';
     
     // ловим асинхронные ошибки
     window.addEventListener('error', (event) => {
@@ -91,9 +141,9 @@ const html = `
     try {
       // безопасно выполняем eval, так как он в своей песочнице <iframe> без доступа на уровень выше
       eval(code);
-      // если нечего показывать, то отобразим транспиляцию в ES6 код
-      if (root.innerHTML === '' && transpiled.length) {
-        root.innerHTML = '<pre><b>Transpiled to ES6</b><hr/>$1</pre>'.replace('$1', transpiled);
+      // отобразим транспиляцию в ES6 код
+      if (transpiled.length > 0) {
+        document.querySelector('#transpiled').innerHTML = transpiled;
       }
     } catch (err) {
       // ловим синхронные ошибки
